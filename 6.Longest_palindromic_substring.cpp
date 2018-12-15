c774f77f7efc6b2dab150dd409d27cb7869c79d2
hiteshkaushik28 <hiteshkaushik545@gmail.com>
2018-12-15 21:40:11 +0530

    Added DP solution also

--- 6.Longest_palindromic_substring.cpp
+++ 6.Longest_palindromic_substring.cpp
@@ -33,3 +33,47 @@ public:
         return s.substr(left, max_len);
     
     }
+
+/* Another nice solution using DP */
+Class Solution{
+public:
+    string longestPalindrome(string s){
+    int len = s.length();
+    if(len < 2)
+        return s;
+    int start = 0;
+    int max_len = 1;
+    int dp[len][len]; // to store if some part of the current substring is also palindrome
+    memset(dp, 0, sizeof(dp));
+
+    for(int i = 0; i < len; ++i)
+    {
+        dp[i][i] = 1; // all single characters are palindromes
+        if(i + 1 < len && s[i] == s[i + 1]) // marking all 2 adjacement same characters
+        {
+            dp[i][i + 1] = 1;
+            start = i;
+            max_len = 2;
+        }
+    }
+
+    for(int k = 3; k <= len; ++k) // this loop is for length of palindrome. Since we marked for length 2 above, k:from 3 to len.
+    {
+        for(int i = 0; i < n-k+1; ++i) // this loop iterates over the input string.
+        {
+            int j = i + k - 1; // we get the ending index of the substring that starts from i and spans till i+k characters.
+
+            if(dp[i+1][j-1] && s[i] == s[j]) // here we use precomputed results to check if substring of this string is already
+                                            //seen as palindrome or not
+            {
+                dp[i][j] = 1;
+                start = i;
+
+                max_len = max(max_len, k); // we keep updating the max length seen so far
+            }
+        }
+    }
+
+    return s.substr(start, max_len);
+    }
+};      
